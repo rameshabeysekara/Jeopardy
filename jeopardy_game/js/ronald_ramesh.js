37d0355e042cc4f8211a60337bc43628b185000b
rbagaporo <r_bagaporo@fanshaweonline.ca>
2023-07-25 12:41:41 -0400

    adding comment in each file : /** */

---
+++ jeopardy_game/js/ronald_ramesh.js
@@ -0,0 +1,15 @@
+/**
+*   STUDENT NAME :  Bandara, Ramesh
+*                    Bagaporo Jr., Ronald
+*   SUBJECT :       INFO-6122-01    |   Programming Javascript
+*   Project :       Jeopardy's
+*
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Your JavaScript code here
+    const JEOPARDY_API = "https://jservice.io/api/clues";
+    var divGameplay = document.getElementById("gameplay");
+    divGameplay.innerHTML = "HELLO!";
+  });
+  
