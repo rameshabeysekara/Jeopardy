9122ee2671b90f5d466d03317f3925b139dc1a53
rbagaporo <r_bagaporo@fanshaweonline.ca>
2023-07-26 02:46:20 -0400

    push!!

--- jeopardy_game/js/ronald_ramesh.js
+++ jeopardy_game/js/ronald_ramesh.js
@@ -7,7 +7,7 @@
 */
 
 document.addEventListener("DOMContentLoaded", () => {
-    
+
     let divFirstSec = document.createElement("div");
     divFirstSec.id = "divFirstSection";
     divFirstSec.classList.add("divFirstSection");
@@ -91,18 +91,18 @@ document.addEventListener("DOMContentLoaded", () => {
     divSecondSecRight.appendChild(h3GrProj)
     h3GrProj.innerHTML = "Group Project ( 25% )"
 
-    
+
     //= = = = = BOTTOM PART SECTION!!
     //= = = = = BOTTOM PART SECTION!!
 
 
-    
+
 
     let divThirdSec = document.createElement("div");
     divThirdSec.id = "divThirdSection";
     divThirdSec.classList.add("divThirdSection");
     document.body.appendChild(divThirdSec);
-    divThirdSec.innerHTML = "Please enter how many categories"
+    divThirdSec.innerHTML = "Please enter how many questions"
 
     let inputCateg = document.createElement("input")
     inputCateg.id = "inputCategory"
@@ -120,58 +120,203 @@ document.addEventListener("DOMContentLoaded", () => {
     //= = = = = THIS IS THE PART!!
     //= = = = = THIS IS THE PART!!
 
-    
 
-    let divBaseBoard = document.createElement ( "div" )
+
+    let divBaseBoard = document.createElement("div")
     divBaseBoard.id = "divBBoard"
-    divBaseBoard.classList.add( "divBBoard" )
+    divBaseBoard.classList.add("divBBoard")
     document.body.appendChild(divBaseBoard);
 
-    
+    let divGameBoard = document.createElement("div");
+    divGameBoard.id = "divGBoard";
+    divGameBoard.classList.add("divGBoard");
+    divBaseBoard.appendChild(divGameBoard);
+    // divGameBoard.innerHTML = "THIS IS IT!"
 
-    const JEOPARDY_API = "https://jservice.io/api/random";
+
+
+    const JEOPARDY_API = "https://jservice.io/api/category";
+
+    // https://jservice.io/api/category?id=1500
+    const JEOPARDY_API_CATEG = "https://jservice.io/api/category"
 
     var HTTP = new XMLHttpRequest()
 
-    btnSubmit.addEventListener("click", categories);
+    btnSubmit.addEventListener("click", categQuestions);
 
-    function categories() {
-        
-        let userInput = inputCateg.value;
-        // console.log(userInput);
-        // divGameBoard.innerHTML = userInput
-        URL = `${JEOPARDY_API}?count=${userInput}`
+    function categQuestions() {
 
-        HTTP.open("GET",URL,true)
+        function categories() {
 
-        HTTP.onload=()=>
-        {
-            let response = JSON.parse( HTTP.responseText )
-            console.log(response)
+            // count = 6, minValue = 1, maxValue = 500
+            const randomValues = getRandomCategory(6, 1, 500);
 
-            for ( question  of response )   {
-                Create_Div( question )
+            function getRandomCategory(count, minValue, maxValue) {
+                const randomValues = [];
+
+                for (let i = 0; i < count; i++) {
+                    const randomValue = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+                    randomValues.push(randomValue);
+                }
+
+                return randomValues;
+            }
+            // console.log(randomValues);
+
+            for (let value of randomValues) {
+
+                // console.log("Category ID:" + value); // You can perform any action with each value here
+
+                const URL = `${JEOPARDY_API_CATEG}?id=${value}`;
+
+                const HTTP = new XMLHttpRequest();
+                HTTP.open("GET", URL, true);
+
+                HTTP.onload = () => {
+                    let response = JSON.parse(HTTP.responseText);
+                    // console.log(response.title);
+                    console.log(response);
+                  
+                    if (response.clues && Array.isArray(response.clues)) {
+                        Create_Div( response ); // Pass the title to the Create_Div function
+                    } else {
+                      console.log("Invalid response or missing clues property.");
+                    }
+                  }
+                  HTTP.send();
+                  
+
+                // HTTP.onload = () => {
+
+                //     let response = JSON.parse(HTTP.responseText);
+                //     console.log( response )
+                //     // console.log(response.title);
+
+                //         Create_Div( response );
+
+                // };
+                // HTTP.send();
             }
         }
-        HTTP.send()
+
+        categories();
+
+        function Create_Div(Data) {
+
+            let divNum = document.createElement("div");
+            divNum.id = "divNumber";
+            divNum.classList.add("divNumber");
+            divNum.innerHTML = Data.title + " Category"
+            divGameBoard.appendChild(divNum);
+
+            Get_Question();
+
+            // let btnQuestion = document.createElement ( "button" )
+            // btnQuestion.id = "button-question"
+            // btnQuestion.classList.add( "button-question" )
+            // // btnQuestion.innerHTML = "100"
+            // btnQuestion.innerHTML = Data.value
+            // divNum.appendChild( btnQuestion );
+
+        }
+
+        function Get_Question() {
+
+            let btnQuestion = document.createElement ( "button" )
+            btnQuestion.id = "button-question"
+            btnQuestion.classList.add( "button-question" )
+            // btnQuestion.innerHTML = "100"
+            btnQuestion.innerHTML = Data.value
+            divNum.appendChild( btnQuestion );
+
+        }
+
+        
+
+
+
+
+
+        // function categories() {
+
+        //     function getRandomCategory(count, minValue, maxValue) {
+        //         const randomValues = [];
+
+        //         for (let i = 0; i < count; i++) {
+        //             const randomValue = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+        //             randomValues.push(randomValue);
+        //         }
+
+        //         return randomValues;
+        //     }
+
+        //     // count = 6, minValue = 1, maxValue = 500
+        //     const randomValues = getRandomCategory(6, 1, 500);
+        //     console.log(randomValues);
+
+        //     URL = `${JEOPARDY_API_CATEG}?id=${randomValues}`
+
+        //     HTTP.open("GET",URL,true)
+
+        //     HTTP.onload=()=>
+        //     {
+        //         let response = JSON.parse( HTTP.responseText )
+        //         console.log(response)
+
+        //         for ( category_id  of response )   {
+        //             Create_Div( category_id )
+        //         }
+        //     }
+        //     HTTP.send()
+
+        // }
+
+        // categories();
+
+        //     let userInput = inputCateg.value;
+        //     // console.log(userInput);
+        //     // divGameBoard.innerHTML = userInput
+        //     URL = `${JEOPARDY_API}?count=${userInput}`
+
+        //     HTTP.open("GET", URL, true)
+
+        //     HTTP.onload = () => {
+        //         let response = JSON.parse(HTTP.responseText)
+        //         console.log(response)
+
+        //         for (question of response) {
+        //             Create_Div(question)
+        //         }
+        //     }
+        //     HTTP.send()
+
+        // }
+
+
+
+        // // FOR 6 - CATEGORIES SECTION
+        // let divQty = new Array(6);
+
+        // for (let i = 0; i < divQty.length; i++) {
+
+        // }
+
+
+
+
+
+        // function Create_Div(Data) {
+
+
+
+        //     let que = document.createElement("div")
+        //     que.id = "idQuestion"
+        //     que.innerHTML = Data.question
+        //     divGameBoard.append(que)
 
     }
-    function Create_Div( Data ) {
 
-        let divGameBoard = document.createElement("div");
-        divGameBoard.id = "divGBoard";
-        divGameBoard.classList.add("divGBoard");
-        divBaseBoard.appendChild(divGameBoard);
-        // divGameBoard.innerHTML = "THIS IS IT!"
 
-        let que = document.createElement( "div" )
-        que.id = "idQuestion"
-        que.innerHTML = Data.question
-        divGameBoard.append(que)
-
-    }
-
-    
 
 
 
